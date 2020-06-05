0bc1a650abfeb1e3ec98122f6037f730fe9e001f
FindZach <zach@findzach.com>
2020-06-05 01:50:13 -0400

    Initial Commit of Complexity OSRS

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/mining/PickaxeType.kt
@@ -0,0 +1,11 @@
+package gg.rsmod.plugins.content.skills.mining
+
+import gg.rsmod.plugins.api.cfg.Items
+
+/**
+ * @author Zach <zach@findzach.com>
+ */
+enum class PickaxeType(val itemId: Int, val level: Int, val anim: Int) {
+
+    BRONZE_PICKAXE(itemId = Items.BRONZE_PICKAXE, level = 1, anim = 102)
+}
